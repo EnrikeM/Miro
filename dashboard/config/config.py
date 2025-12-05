f7eb993c40d58f057dec0c371c738ab57ef1348b
timurkuvatov <tvkuvatov@edu.hse.ru>
2025-12-06 01:25:59 +0300

    add config

---
+++ dashboard/config/config.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent
+
+class RunConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 10000
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn = "postgresql+asyncpg://postgres:postgres@localhost:5442/boards"
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding='utf-8',
+        case_sensitive=False,
+        env_nested_delimiter="_",
+    )
+
+    run: RunConfig = RunConfig()
+    db: DatabaseConfig = DatabaseConfig()
+
+settings = Settings()
